781102e77e79ca1fc162eb9fc0bd0e29743bb13f
Joris <you@example.com>
2021-12-19 13:31:36 +0100

    feat: ios safe area

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,16 @@ useQueryProvider();
 </script>
 
 <template>
-  <div class="pt-16 px-6">
+  <div class="safe-area">
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+.safe-area {
+  padding-top: max(2rem, env(safe-area-inset-top));
+  padding-right: max(1.5rem, env(safe-area-inset-right));
+  padding-bottom: max(1.5rem, env(safe-area-inset-bottom));
+  padding-left: max(1.5rem, env(safe-area-inset-left));
+}
+</style>
